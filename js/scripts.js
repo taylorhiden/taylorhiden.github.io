07c6f0e16d05f05a8e19f114a22734f0bb02dd03
TAYLOR HIDEN <TAYLOR@ALLEGIANCESTRATEGIES.COM>
2016-02-16 17:42:12 -0500

    Added carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// ADVOCACY PAGE
 $('.readlgbt').click(readLgbt);
 $('.readimmigration').click(readImmigration);
 $('.readtax').click(readTax);
@@ -14,3 +15,34 @@ function readImmigration() {
 function readTax() {
 	$('#show_tax_on_click').slideToggle();
 }
+
+
+// DIGITAL PAGE
+
+$(document).ready(function() {
+
+var pictures = ['images/array/Haley.jpg', 'images/array/Margaret quote.jpg', 'images/array/McKinnon.jpg', 'images/array/delegates post-IA.jpg'];
+var currentImage = 0;
+
+$('#nextbtn').click(function() {
+	event.preventDefault();
+	currentImage = currentImage + 1;
+
+	if (currentImage == pictures.length) {
+           currentImage = 0;
+       }
+
+$('#facebook_graphics').attr('src', pictures[currentImage]);
+   });
+
+$('#previousbtn').click(function() {
+       currentImage = currentImage - 1;
+
+ if (currentImage < 0) {
+     currentImage = pictures.length - 1;
+       }
+
+ $('#facebook_graphics').attr('src', pictures[currentImage]);
+   });
+
+});
